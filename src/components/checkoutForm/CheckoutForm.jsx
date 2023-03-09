de59eb4a99e7ca42f4d196f3145106e0e54631d6
hvbtrung <hvbtrung@gmail.com>
2023-03-10 01:53:01 +0700

    change url

--- src/components/checkoutForm/CheckoutForm.jsx
+++ src/components/checkoutForm/CheckoutForm.jsx
@@ -60,7 +60,8 @@ const CheckoutForm = () => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:5173/success",
+        // return_url: "http://localhost:5173/success",
+        return_url: "https://btfiverr-client-jfxuptwg2-hvbtrung.vercel.app/success",
       },
     });
 
